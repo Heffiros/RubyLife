ff74c7fa8497e556f3221e45415af1fbe07df9e1
Heffiros <vasax.levy@gmail.com>
2014-04-05 11:47:03 +0200

    patch

--- life.rb
+++ life.rb
@@ -1,72 +1,88 @@
+RANGE = 0..9
+DISPLAY = [" ", "*"]
+
 def create_tab
-		life_tab = Array.new(50){Array.new(50){[0,1].sample}}
-		return life_tab
+	#Create the rand Tab
+	life_tab = Array.new(10){Array.new(10){[0,1].sample}}
 end
 
 def aff_tab(life_tab)
-	j = 0
-	while (j <= 49)
-			i = 0
-			while (i <= 49)
-					if life_tab[j][i] == 0
-						print " "
-					end
-					if life_tab[j][i] == 1
-						print "*"
-					end
-					i = i + 1 
-				end
-				puts "\n"
-				j = j + 1
-				
+	#write the tab
+	RANGE.each do |j|
+		RANGE.each do |i|
+			if (life_tab[j][i] == 0)
+				print " "
+			else
+				print "*"
+			end
 		end
+			print "\n"
+	end
+	#life_tab.map { |ary| puts ary.map{|i| DISPLAY[i] }.join('') }
 end
 
 def clone_tab(life_tab)
-	clone = Array.new(50){Array.new(50){[0,1].sample}}
-	j = 0
-	while (j <= 49)
-		i = 0
-			while (i <= 49)
-				clone[j][i] = life_tab[j][i]
-				i = i + 1
-			end
-			j = j + 1
+	#do a copy of the tab
+	clone = Array.new(10){Array.new(10){[0,1].sample}}
+	RANGE.each do |j|
+		RANGE.each do |i|
+			clone[j][i] = life_tab[j][i]
 		end
-		return clone
+	end
+	return clone
 end
 
-def game(life_tab, clone)
-	j = 0
-	while (j <= 49)
-		i = 0
-		while (i <= 49)
+def game(life_tab)
+	#main function for check the tab
+	clone = clone_tab(life_tab)
+	RANGE.each do |j|
+		RANGE.each do |i|
+			around = check_around(life_tab, i, j)
 			if life_tab[j][i] == 1
-				if check_around(life_tab, i , j) < 2 || check_around(life_tab, i ,j) > 3 
+				if around < 2 || around > 3
 					clone[j][i] = 0
+				elsif around == 3
+					clone[j][i] = clone[j][i]
 				end
-				elsif check_around(life_tab, i , j) == 3
-					clone[j][i] = 1
-				end
-			end
 			elsif life_tab[j][i] == 0
-				if check_around(life_tab, i, j) == 3
+				if around == 3
 					clone[j][i] == 1
-				end
 				else
 					clone[j][i] == 0
 				end
 			end
 		end
-	end	
-	return clone				
+	end
+	print "\n-------------\n"
+	aff_tab clone
+	print "-------------\n"
+	sleep 1
+	return clone
+end
+
+def check_around(life_tab, i , j)
+	#check alive cell
+	a = j - 1
+	count = 0
+	while (a < j + 1)
+		b = i - 1
+		while (b <= i + 1)
+			if life_tab[a][b] == 1
+				count = count + 1
+			end
+			b = b + 1
+		end
+		a = a + 1
+	end
+	count = count - life_tab[j][i]
+	return count
+
 end
 
 var = create_tab
-var2 = clone_tab(var)
 while (1)
-	aff_tab(var)
-
+	var = game(var)
+	system("cls")
 end
 
 
